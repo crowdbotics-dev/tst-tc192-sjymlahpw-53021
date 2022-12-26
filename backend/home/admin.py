2d89c1a606045e362a12f9d3c643a3f6efd2c1fd
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-26 10:39:40 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Asdfgh
+admin.site.register(Asdfgh)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Asdfgh(models.Model):
+    'Generated Model'
+    ewrtyui = models.BigIntegerField()
